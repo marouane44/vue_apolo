94914e4f9631e0e0cc1a7c536c67d2c44a5a448f
marwanhaz43 <marwanhaz43@gmail.com>
2023-04-09 21:46:11 +0000

    teeeee

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
   devServer: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000/",
+        target: "https://master--remarkable-mooncake-fc9763.netlify.app/",
         changeOrigin: true,
         pathRewrite: {
           "^/api": "",
